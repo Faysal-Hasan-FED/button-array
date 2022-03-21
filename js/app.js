ac4c6a34a588cc06ae56a58fc5ae86dac72380b2
Faysal <frontendfaysal@gmail.com>
2022-03-21 14:26:05 +0600

    solved

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// display one array item at a time 
+
+const myTexts = ['Faysal' , 'Hasan' , 'Bangladesh'];
+const textArea = document.getElementById('my-text');
+
+document.getElementById('my-btn').addEventListener('click',()=>{
+    let i =0;
+
+    setInterval(()=> {            
+    document.getElementById('my-text').innerHTML = myTexts[i++];    
+    if (i == myTexts.length) i = 0;   
+}, 1000); 
+
+})
+
--- js/main.js
+++ js/main.js
@@ -1,14 +1,42 @@
+// display array items one after one by clicking on the button
 const  texts = ["Faysal", "Hasan" , "Bangladesh"];
 const  myText = document.getElementById("my-text");
 let  index = 0;
 
 myText.innerHTML = texts[0];
 
-document.getElementById('my-btn').addEventListener('click',()=>{
-    setTimeout(()=>{
-        index++;
-    index %= texts.length;
-    myText.innerHTML = texts[index];
-    },1000)
-})
+const handleClick = ()=>{
+    
+    
+        setTimeout(()=>{
+            index++;
+        index %= texts.length;
+        myText.innerHTML = texts[index];
+        },200)
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById('my-btn').addEventListener('click',()=>{
+    
+// })
 
